32f768c3c7329b4da4d90919e2459ce8116c296f
Hugo Calmels <calmels.hugo@yahoo.fr>
2022-06-16 16:51:41 +0200

    added #confirmable

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
-  devise :database_authenticatable,
-  :jwt_authenticatable,
-  :registerable,
-  jwt_revocation_strategy: JwtDenylist
+  devise :database_authenticatable, :registerable,
+  :recoverable, :rememberable, :validatable, :confirmable,
+  :jwt_authenticatable, jwt_revocation_strategy: JwtDenylist
 end
